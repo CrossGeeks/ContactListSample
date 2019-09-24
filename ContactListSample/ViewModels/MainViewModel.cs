a9066e15a47c82f318f432beefa28f486c43fc71
Rendy  Del Rosario Campusano <rendy.delrosario@gmail.com>
2019-09-24 08:38:35 -0400

    Added search filter

--- ContactListSample/ViewModels/MainViewModel.cs
+++ ContactListSample/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ContactListSample.Models;
@@ -15,15 +17,33 @@ namespace ContactListSample.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
 
         public string Title => "Contacts";
+
+        public string SearchText { get; set; }
         public ObservableCollection<Contact> Contacts { get; set; }
+        public ObservableCollection<Contact> FilteredContacts
+        {
+            get
+            {
+                return string.IsNullOrEmpty(SearchText) ? Contacts : new ObservableCollection<Contact>(Contacts?.ToList()?.Where(s => !string.IsNullOrEmpty(s.Name) && s.Name.ToLower().Contains(SearchText.ToLower())));
+            }
+        }
         public MainViewModel(IContactsService contactService)
         {
             _contactService = contactService;
             Contacts = new ObservableCollection<Contact>();
+            Xamarin.Forms.BindingBase.EnableCollectionSynchronization(Contacts, null, ObservableCollectionCallback);
             _contactService.OnContactLoaded += OnContactLoaded;
-
             LoadContacts();
-           
+        }
+
+
+        void ObservableCollectionCallback(IEnumerable collection, object context, Action accessMethod, bool writeAccess)
+        {
+            // `lock` ensures that only one thread access the collection at a time
+            lock (collection)
+            {
+                accessMethod?.Invoke();
+            }
         }
 
         private void OnContactLoaded(object sender, ContactEventArgs e)
